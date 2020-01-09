23733b5c89142e58c9c13fd73b7d5c7668540466
Krystyna Koryagina <krystyna.koryagina@gmail.com>
2020-01-09 19:30:18 +0200

    finished mavic animation

--- mavic/script.js
+++ mavic/script.js
@@ -1,22 +1,20 @@
 $(function() {
 	const img = $('.mavic-img');
-
 	
 	$('.mavic').hover(
 		function() {
-			const $this = $(this);
-
-			img.addClass('mavic-off')
-			   .removeClass('mavic-on');
-		},
-		function() {
-
-			const $this = $(this);
+			$(this).addClass('animate-on')
+				   .removeClass('animate-off');
 
 			img.addClass('mavic-on')
 			   .removeClass('mavic-off');
+		},
+		function() {
+			$(this).addClass('animate-off')
+				   .removeClass('animate-on');
+
+			img.addClass('mavic-off')
+			   .removeClass('mavic-on');
 		}
 	);
-
-
 });
